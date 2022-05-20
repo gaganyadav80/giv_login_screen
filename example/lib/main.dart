ac8a396e2523c3ae5a2cf973560bfa659bfa699e
gaganyadav80 <gaganyadav80@gmail.com>
2022-05-20 07:55:49 +0530

    feat: adds example app to plugin

---
+++ example/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:giv_login_screen/giv_login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GivLoginScreen();
+  }
+}
--- lib/giv_login_screen.dart
+++ lib/giv_login_screen.dart
@@ -1,7 +1,12 @@
 library giv_login_screen;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class GivLoginScreen extends StatelessWidget {
+  const GivLoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
